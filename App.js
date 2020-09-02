01e7c1dbae63a7d15920574696c7624abbfc4db2
sanju <37176975+sanjugitstock@users.noreply.github.com>
2020-09-02 15:03:29 +0530

    changes in APP.js  for short code
    how the use of object distructring... and hooks fn in from

--- App.js
+++ App.js
@@ -2,91 +2,25 @@ import React, { useState } from 'react';
 import './index.css';
 
 const App = () => {
-const [fullName,UpdatedName] = useState({
-  fname:'',
-  lname:'',
-  email:'',
-  phone:'',
-});
-
-
+  const [name,UpdatedName] = useState()
+  const [clikname,fullname]= useState()
   const inputValue = (event) =>{
-    // console.log(event.target.value);
-    // UpdatedName(event.target.value);
-    const value = event.target.value;
-    const name = event.target.name;
-    // const email = event.target.email;
-    // const phone = event.target.phone;
-  
-    UpdatedName( (preValue) => {
-      if(name === "fname"){
-        return{
-          fname: value,
-          lname:preValue.lname,
-          phone:preValue.phone,
-          email: preValue.email ,
-        }
-          } else if (name === "lname"){
-            return {
-              fname: preValue.fname,
-              lname: value ,
-              phone:preValue.phone,
-              email: preValue.email ,
-        }
-      }else if (name === "email"){
-        return {
-          fname: preValue.fname,
-          lname: preValue.lname ,
-          email: value,
-          phone:preValue.phone,
-          }
-        }else if (name === "phone"){
-          return {
-            fname: preValue.fname,
-            lname: preValue.lname ,
-            email: preValue.email ,
-            phone: value ,
-
-         }   
-      }  
-
-    })
+      // console.log(event.target.value);
+      UpdatedName(event.target.value);
   }
-  const setInput = (event) =>{
-   // event.preventDefault();
-    alert('your form get submitted');
-  };
-
+  const setInput = () =>{
+    fullname(name);
+  }
   return(
    <>
    <div>
-     <form onSubmit={setInput} >
-   <h1> hello {fullName.fname} {fullName.lname} {fullName.email} {fullName.phone}  </h1>
-
+   <h1> hello {clikname} </h1>
    <input placeholder="enter your name" 
    onChange={inputValue}
-   value={fullName.fname}
-   name= 'fname'
-    />
-    <input placeholder="password" onChange={inputValue}
-    value={fullName.lname}
-    name="lname"
-    />
-
-   
-    <input placeholder="Email" onChange={inputValue}
-    value={fullName.email}
-    name="email"
-    />
-   
-    <input placeholder="phone" onChange={inputValue}
-    value={fullName.phone}
-    name="phone"
-    />
-   <button >submit</button>
-   </form>
+   value={name}
+    ></input>
+   <button onClick={setInput}>submit</button>
    </div>
-   
    </>
   )
 };
